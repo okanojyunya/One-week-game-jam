2e11697fd20a5610dfe3c636a45c1c5747fa0435
okanojyunya <monogamingvantan@gmail.com>
2024-09-02 14:48:18 +0900

    各プレハブ化、敵を自動生成するコンポーネント作成＊プレイヤーが消えると敵の移動するスクリプトにエラーが起こる

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 一定間隔で敵を生成するコンポーネント
+/// </summary>
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject m_enemyPrefab;
+    [SerializeField] int m_interval;
+    float m_time;//経過時間
+
+    void Update()
+    {
+        m_time += Time.deltaTime;
+        if(m_time > m_interval)
+        {
+            m_time = 0;
+            Instantiate(m_enemyPrefab, gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
